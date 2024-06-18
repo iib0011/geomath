39e614554f2fdfb2dade3f9ac9b3a78fab7697bd
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2024-06-18 08:38:26 +0100

    feat: infinite zoom init

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,69 +1,133 @@
-import Avatar from 'components/Avatar'
-import logo from 'assets/logo.svg'
-
-const randoms = [
-  [1, 2],
-  [3, 4, 5],
-  [6, 7]
-]
+import { useEffect, useRef } from 'react'
+import { Grid } from './Grid/IGrid'
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (canvas) {
+      const context = canvas.getContext('2d', { alpha: false })
+
+      if (context == null) return
+      let grid: Grid
+      let mouseStartX: number
+      let mouseStartY: number
+
+      let mousePosX = 0
+      let mousePosY = 0
+
+      ///////////////////////////////////////////////////////////////////////////////////////////
+
+      ///////////////////////////////////////////////////////////////////////////////////////////
+
+      // Fonctions
+
+      const onResize = () => {
+        // Déterminer la résolution d'affichage
+
+        const stdCanvasW = document.body.clientWidth - 2 * canvas.offsetLeft
+        const stdCanvasH = stdCanvasW / 2
+        const optCanvasW = stdCanvasW * window.devicePixelRatio
+        const optCanvasH = stdCanvasH * window.devicePixelRatio
+
+        if (window.devicePixelRatio > 1) {
+          canvas.width = optCanvasW
+          canvas.height = optCanvasH
+          context.scale(window.devicePixelRatio, window.devicePixelRatio)
+        } else {
+          canvas.width = stdCanvasW
+          canvas.height = stdCanvasH
+        }
+
+        canvas.style.width = stdCanvasW + 'px'
+        canvas.style.height = stdCanvasH + 'px'
+
+        // Créer et afficher la grille
+        grid = new Grid({
+          stdCanvasH,
+          stdCanvasW,
+          canvas,
+          context,
+          minCellSize: 20
+        })
+        grid.draw()
+      }
+
+      ///////////////////////////////////////////////////////////////////////////////////////////
+
+      // Démarrage de la webapp
+
+      onResize()
+
+      ///////////////////////////////////////////////////////////////////////////////////////////
+
+      // Evenements
+
+      window.addEventListener('resize', onResize)
+
+      // Zoomer le canvas avec la roulette
+      canvas.addEventListener('wheel', function (e) {
+        e.preventDefault()
+        e.stopPropagation()
+
+        const zoom = -e.deltaY / 120
+        grid.setZoom(zoom, mousePosX, mousePosY)
+      })
+
+      // Pan canvas on drag
+
+      canvas.addEventListener('mousedown', function (e) {
+        e.preventDefault()
+        e.stopPropagation()
+
+        mouseStartX = e.clientX - canvas.offsetLeft
+        mouseStartY = e.clientY - canvas.offsetTop
+
+        canvas.onmousemove = function (e) {
+          e.preventDefault()
+          e.stopPropagation()
+
+          grid.setPan(mouseStartX, mouseStartY, mousePosX, mousePosY)
+        }
+      })
+
+      //  Récupérer les coordonnées de la souris en mouvement.
+      canvas.addEventListener('mousemove', function (e) {
+        e.preventDefault()
+        e.stopPropagation()
+
+        mousePosX = e.clientX - canvas.offsetLeft
+        mousePosY = e.clientY - canvas.offsetTop
+      })
+
+      canvas.addEventListener('mouseup', function (e) {
+        e.preventDefault()
+        e.stopPropagation()
+
+        canvas.onmousemove = null
+        canvas.onwheel = null
+
+        // Get last (0;0) coordinates
+        grid.lastAxisPosX = grid.axisPosX
+        grid.lastAxisPosY = grid.axisPosY
+      })
+
+      canvas.addEventListener('mouseout', function (e) {
+        e.preventDefault()
+        e.stopPropagation()
+
+        canvas.onmousemove = null
+        canvas.onwheel = null
+
+        // Get last (0;0) coordinates
+        grid.lastAxisPosX = grid.axisPosX
+        grid.lastAxisPosY = grid.axisPosY
+      })
+    }
+  }, [])
   return (
-    <div className="relative overflow-hidden bg-white">
-      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-          <div className="sm:max-w-lg">
-            <div className="my-4">
-              <Avatar size="large" src={logo} />
-            </div>
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Welcome!
-            </h1>
-            <p className="mt-4 text-xl text-gray-500">
-              This is a boilerplate build with Vite, React 18, TypeScript,
-              Vitest, Testing Library, TailwindCSS 3, Eslint and Prettier.
-            </p>
-          </div>
-          <div>
-            <div className="my-10">
-              <a
-                href="vscode://"
-                className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2"
-              >
-                Start building for free
-              </a>
-              <div
-                aria-hidden="true"
-                className="pointer-events-none mt-10 md:mt-0 lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
-              >
-                <div className="absolute sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                  <div className="flex items-center space-x-6 lg:space-x-8">
-                    {randoms.map((random, number) => (
-                      <div
-                        key={`random-${random[number]}`}
-                        className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8"
-                      >
-                        {random.map((number) => (
-                          <div
-                            key={`random-${number}`}
-                            className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100"
-                          >
-                            <img
-                              src={`https://picsum.photos/600?random=${number}`}
-                              alt=""
-                              className="size-full bg-indigo-100 object-cover object-center"
-                            />
-                          </div>
-                        ))}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <canvas ref={canvasRef}></canvas>
     </div>
   )
 }
